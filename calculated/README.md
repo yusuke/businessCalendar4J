e025631294277af687bb58eefe4f867add644b11
Yusuke Yamamoto <yusuke@mac.com>
2021-01-29 21:50:20 +0900

    算出済第一/最終営業日

---
+++ calculated/README.md
@@ -0,0 +1,6 @@
+# 事前計算済祝休日/営業日
+## japan-first-last-business-days-[from]-[to]-[encoding].csv
+1955年から向こう9年先まで、土日を休業日とした場合の毎月の第一営業日、最終営業日を計算したもの
+
+## japan-holidays-[from]-[to]-[encoding].csv
+1955年から向こう9年先まで、祝休日を計算したもの。
--- src/test/java/com/samuraism/holidays/第一最終営業日ダンプ.java
+++ src/test/java/com/samuraism/holidays/第一最終営業日ダンプ.java
@@ -24,28 +24,37 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.samuraism.holidays.日本の祝休日.土日休業;
+
 /**
- * 来年以降の祝休日を向こう9年間計算してファイルにダンプ
+ * 第一営業日/最終営業日を向こう9年間計算してファイルにダンプ
  */
-public class 日本の祝休日ダンプ {
+public class 第一最終営業日ダンプ {
     public static void main(String[] args) {
         final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/M/d");
         final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-d");
         final LocalDate start = LocalDate.of(1955, 1, 1);
         final LocalDate now = LocalDate.now();
         final LocalDate end = LocalDate.of(now.getYear() + 9, 12, 31);
-        final String shiftJisFileName = String.format("calculated/japan-holidays%s-%s-Shift_JIS.csv", start.format(formatter), end.format(formatter));
-        final String utf8FileName = String.format("calculated/japan-holidays%s-%s-UTF8.csv", start.format(formatter), end.format(formatter));
+        final String shiftJisFileName = String.format("calculated/japan-first-last-business-days%s-%s-Shift_JIS.csv", start.format(formatter), end.format(formatter));
+        final String utf8FileName = String.format("calculated/japan-first-last-business-days%s-%s-UTF8.csv", start.format(formatter), end.format(formatter));
         try (final BufferedWriter shiftJIS = Files.newBufferedWriter(Paths.get(shiftJisFileName), Charset.forName("Shift_JIS"));
              final BufferedWriter utf8 = Files.newBufferedWriter(Paths.get(utf8FileName), StandardCharsets.UTF_8)        ) {
-            final String header = "国民の祝日・休日月日,国民の祝日・休日名称\n";
+            final String header = "年月日,営業日\n";
             shiftJIS.write(header);
             utf8.write(header);
-            for (祝休日 holiday : new 日本の祝休日().get指定期間内の祝休日️(start, end)) {
-                final String line = String.format("%s,%s\n", holiday.日付.format(dateTimeFormatter), holiday.名称);
+            LocalDate cursor = start;
+            日本の祝休日 holidays = new 日本の祝休日().add祝休日(土日休業);
+            while (cursor.isBefore(end)) {
+                LocalDate 月末 = cursor.withDayOfMonth(cursor.lengthOfMonth());
+                LocalDate 最初営業日 = holidays.最初の営業日(cursor);
+                LocalDate 最終営業日 = holidays.最後の営業日(月末);
+                final String line = String.format("%s,%s\n%s,%s\n",最初営業日.format(dateTimeFormatter),"第一営業日",
+                        最終営業日.format(dateTimeFormatter), "最終営業日");
                 System.out.print(line);
                 shiftJIS.write(line);
                 utf8.write(line);
+                cursor = cursor.plusMonths(1);
             }
         } catch (IOException e) {
             e.printStackTrace();
