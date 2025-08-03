0f1e2443a158b6d135b29fcbfce9d8e21f4d919b
Yusuke Yamamoto <yusuke@mac.com>
2025-08-03 17:06:43 +0900

    Update Gradle version to 9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
